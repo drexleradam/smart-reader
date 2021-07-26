a8fdd059e1d2dfaa9002f55e8305b74914f0fdf4
drexleradam <drexler1333@gmail.com>
2021-07-26 23:48:04 +0200

    beta version ready

---
+++ src/main/java/hu/big/brain/csv/config/SmartReaderJobConfiguration.java
@@ -0,0 +1,49 @@
+package hu.big.brain.csv.config;
+
+import hu.big.brain.csv.model.Person;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class SmartReaderJobConfiguration {
+
+    public JobBuilderFactory jobBuilderFactory;
+
+    public StepBuilderFactory stepBuilderFactory;
+
+    public SmartReaderJobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Job importUserJob(Step step) {
+        return jobBuilderFactory.get("smart-load-csv")
+                .incrementer(new RunIdIncrementer())
+                .flow(step)
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step(ItemReader<Person> reader,
+                     ItemProcessor<Person, Person> processor,
+                     JdbcBatchItemWriter<Person> writer) {
+        return stepBuilderFactory.get("smart-load")
+                .<Person, Person>chunk(100)
+                .reader(reader)
+                .processor(processor)
+                .writer(writer)
+                .build();
+    }
+}
---
+++ src/main/java/hu/big/brain/csv/reader/SmartPersonCsvReader.java
@@ -0,0 +1,29 @@
+package hu.big.brain.csv.reader;
+
+import hu.big.brain.csv.model.Person;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.separator.SimpleRecordSeparatorPolicy;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.PathResource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SmartPersonCsvReader extends FlatFileItemReader<Person> {
+
+    public SmartPersonCsvReader() {
+        this.setName("smart-person-csv-reader");
+        this.setLineMapper(new DefaultLineMapper<>());
+        this.setResource(new PathResource("feladat_1.csv"));
+        this.setLinesToSkip(1);
+        DefaultLineMapper<Person> mapper = new DefaultLineMapper<>();
+        mapper.setFieldSetMapper(new SmartPersonFieldSetter());
+        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+        tokenizer.setDelimiter("\t");
+        tokenizer.setNames("first_name","last_name","age");
+        mapper.setLineTokenizer(tokenizer);
+        this.setLineMapper(mapper);
+    }
+
+}
