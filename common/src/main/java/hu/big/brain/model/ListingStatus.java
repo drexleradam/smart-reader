d3935f40b73f29e653ddd52101b86fab9497e9cc
drexleradam <drexler1333@gmail.com>
2021-10-07 00:07:51 +0200

    form changes

---
+++ common/src/main/java/hu/big/brain/model/ListingStatus.java
@@ -0,0 +1,17 @@
+package hu.big.brain.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ListingStatus {
+    private int id;
+    @JsonAlias("status_name")
+    private String name;
+}
